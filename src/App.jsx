40a1b9a6a7866fdd2ad49ea6f336763a8620d04d
crhbala <chrisbala8888@gmail.com>
2023-10-12 14:15:24 +0530

    event handling with form onsubmit

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,30 @@ import Note from './Note';
 
 function App(props) {
   const { notes } = props;
-  
+  const itemList = [];
+
+  notes.forEach((note) => {
+    itemList.push(<Note key={note.id} note={note}/>)
+  });
+
+  const addNote = (event) => {
+    event.preventDefalt();
+    // console.log('button clicked');
+    // console.log(event.target);
+
+    // console.log(event.target.elements.note.value);
+  }
   return (
     <div>
       <h1>Notes</h1>
-      {
-        notes.map((note) => {
-    return <Note key={note.id} note={note} />;
-            
-          })
-      }
+      <ul>
+      {itemList}
+      </ul> 
       
+      <form onSubmit={addNote}>
+        <input name='note'/>
+        <button type='submit'>Save Note</button>
+      </form>
     </div>
   )
 }
