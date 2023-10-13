f13b6ca4a61ff84bf7e2ee82c45bffd6ae2cf74b
crhbala <chrisbala8888@gmail.com>
2023-10-13 17:24:16 +0530

    page re-rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,19 @@
-import { Component } from "react";
-//stateful components
+/*
+Hooks:
+  -any function that start with "use" is called a hook.
+  -they are special function that are only available while react is rendering
 
-class App extends Component{
-  constructor(props) {
-    super(props);
+  To creat a counter, where the value is increased as a function of time or at the click of a button.
+*/
 
-    this.state = {
-      good: 0,
-      neutral: 0,
-      bad: 0,
-      
-    }
-  }
-  goodClick = () => {
-    this.setState((prevState) => ({
-      good : prevState.good + 1,
-    }))
-  }
-  neutralClick = () => {
-    this.setState((prevState) => ({
-      neutral : prevState.neutral + 1,
-    }))
-  }
-   badClick = () => {
-   this.setState((prevState) => ({
-      bad : prevState.bad + 1,
-    }))
-  }
-  reset = () => {
-    this.setState({
-      good: 0,
-      neutral: 0,
-      bad: 0,
-      
-    })
-  }
+import React from "react";
 
-  render() {
-    return (
-      <div>
-
-        <h1>Give Feedback</h1>
-        
-        <button onClick={this.goodClick}>Good</button>
-        <button onClick={this.neutralClick}>Neutral</button>
-        <button onClick={this.badClick}>Bad</button>
-
-        <h1>Statistics</h1>
-        <p>good: {this.state.good}</p>
-        <p>neutral: {this.state.neutral}</p>
-        <p>bad: {this.state.bad}</p>
-        <button onClick={this.reset}>Reset Feedbacks</button>
-      </div>
-    )
-  }
+function App(props) {
+  // console.log(props);
+  let { counter } = props;
+  return (
+    <div>{counter}</div>
+  )
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -31,5 +31,13 @@ const notes = [
         important: true
     }
 ]
+//creat a variable called counter and initialize the value to 1
+let counter = 1;
+let refresh = () => {
+    ReactDOM.createRoot(document.getElementById('root')).render(<App counter={counter} />)
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+setInterval(() => {
+    refresh();
+    counter += 1;
+},1000)
