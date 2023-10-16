3e2389cc3f53e7a744755890eb88e187179c3c24
crhbala <chrisbala8888@gmail.com>
2023-10-16 15:40:20 +0530

    traditional non  -optinal method to handle routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,43 @@
-import React, {useState} from 'react';
-
-function ChildComponent({onMessage}) {
-  const message = `Hello from Child`;
-
-  return (
-    <>
-      <h2>Child Comonent</h2>
-      <button onClick={()=>onMessage(message)}>Send Message to the parent</button>
-    </>
-  )
-}
+import React, { useState } from 'react';
+import Home from './Home';
+import Notes from './Notes';
+import Users from './Users';
 
 function App() {
 
-  const [message, setMessage] = useState('');
+  const [page, setPage] = useState('home');
 
-  const handleMessage = (data) => {
-    setMessage(data);
+  //higher order function
+  const toPage = (page) => (event) => {
+    event.preventDefault()
+    setPage(page);
+    console.log(page);
   }
+
+  const content = () => {
+    if (page == 'home') {
+      return <Home />
+    }else if(page == 'notes'){
+      return <Notes />
+    }else if(page == 'users'){
+      return <Users />
+    }
+  }
+
+  const padding = {
+    padding: 5
+  }
+
   return (
     <div>
-      <h2>Parent Component</h2>
-      <p>Message from the Child Component: { message}</p>
-      <ChildComponent onMessage={handleMessage} />
+      <div>
+        <a href="" onClick={toPage('home')} style={padding}>home</a>
+        <a href="" onClick={toPage('notes')} style={padding}>notes</a>
+        <a href="" onClick={toPage('users')} style={padding}>users</a>
+      </div>
+      {content()}
     </div>
   )
 }
-
-/*
-  passing data from the parent component to child component:
-
-  parent Component:
-    const age = 25;
-    <child age = {age} name = {'satish'} />
-
-  child component:
-  props ->{
-    age:25,
-    name: 'satish'
-  }
-
-  passing data from the child component to parent component:
-
-    parent Component:
-      const [childAge, setChildAge]= useState(0);
-
-      const handlChildAge(age){
-      setChildAge(age);
-      }
-
-    <child handlChildAge ={handlChildAge} />
-
-  child component:
-  const age = 25;
-  props ->{handlChildAge:handlChildAge}
-  handlChildAge(25);
-  
-*/
 
 export default App;
