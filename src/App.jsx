25a8e73e8e2511928770fb0d4ff95a5ea9d624b0
crhbala <chrisbala8888@gmail.com>
2023-10-17 15:18:11 +0530

    form handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,48 @@
-import React, { createContext, useState } from 'react';
-import ChildComponent from './components/ChildComponent';
+import React, { useState } from 'react';
+import Note from './components/Note';
 
-//create a context  
-const MessageContext = createContext();
+function App(props) {
 
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState('');
 
-function App() {
+  const addNote = (event) => {
+    event.preventDefault();
+    // console.log('button clicked');
+    // console.log(newNote);
 
-  const [message, setMessage]=useState('Hello from App');
+    const noteObject = {
+      id: notes.id + 1,
+      content: newNote,
+      importent: Math.random()<0.5,
+    }
+    setNotes(notes.concat(noteObject));
+    setNewNote('');
+  }
+
+  const handleNoteChange = (event) => {
+    // console.log(event.target.value);
+    setNewNote(event.target.value)
+    // console.log(newNote);
+  }
 
   return (
     <div>
-      <h1>Parent Component</h1>
-      <MessageContext.Provider value={[message,setMessage]}>
-        <ChildComponent />
-      </MessageContext.Provider>
+      <h1>Notes</h1>
+      <ul>
+        {
+          notes.map(note =>
+            <Note key={note.id} note={note}/>)
+        }
+      </ul>
+      <form onSubmit={addNote}>
+        <input placeholder='add new note ...'
+          value={newNote}
+        onChange={handleNoteChange}/>
+        <button type='submit'>save</button>
+      </form>
     </div>
   )
 }
 
-export {App as default, MessageContext};
+export default App
