33eba33513c55ea5622c59dbe3abef096623f810
crhbala <chrisbala8888@gmail.com>
2023-10-17 11:25:25 +0530

    props drillin parent to child and child to grandchild

--- src/components/ChildComponent.jsx
+++ src/components/ChildComponent.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import GrandChildComponent from './GrandChildComponent';
 
-function ChildComponent({message}) {
+function ChildComponent({ message }) {
+    const childMassage = `${message}Hello GrandChild`
   return (
       <div>
           <h2>ChildComponent</h2>
           <p>Message from Parent Component: <b>{message}</b></p>
-          <GrandChildComponent message = {message} />
+          <GrandChildComponent message = {childMassage} />
       </div>
       
   )
