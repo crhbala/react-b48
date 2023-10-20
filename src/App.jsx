fbea2114c918b048feb11ef4ec7969d0c3d4546a
crhbala <chrisbala8888@gmail.com>
2023-10-20 17:13:01 +0530

    tic toc game handleClick function logics

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,47 @@
-import React, { useReducer } from 'react';
+import React from 'react';
+import '../src/App.css';
+import { useState } from 'react';
 
-const initialState = {
-  isActive: false,
-};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case 'toggle':
-      // return { isActive: !state.isActive };
-    return { isActive: state.isActive ? false : true };
-    }
+function Button({value,onClick}) {
+  return (
+     <button className='square' onClick={onClick}>{value}</button>
+   )
 }
 
 function App() {
 
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
 
+  function handleClike(i) {
+    if (squares[i]) {
+    return
+    }
+    const nextSquares = squares.slice();
+    nextSquares[i] = 'X';
+    console.log(nextSquares);
+    setSquares(nextSquares)
+
+ }
+
+  
   return (
     <div>
-      <h2>User Profile</h2>
-      <p>Account Active:{state.isActive?'Yes':'No'}</p>
-      <button onClick={()=>dispatch({type:'toggle'})}>Toggle Profile</button>
+      <div className='row'>
+        <Button value={squares[0]} onClick={()=>handleClike(0)} />
+        <Button value={squares[1]} onClick={()=>handleClike(1)} />
+        <Button value={squares[2]} onClick={()=>handleClike(2)} />
+      </div>
+      <div className='row'>
+        <Button value={squares[3]} onClick={()=>handleClike(3)} />
+        <Button value={squares[4]} onClick={()=>handleClike(4)} />
+        <Button value={squares[5]} onClick={()=>handleClike(5)} />
+      </div>
+      <div className='row'>
+        <Button value={squares[6]} onClick={()=>handleClike(6)} />
+        <Button value={squares[7]} onClick={()=>handleClike(7)} />
+        <Button value={squares[8]} onClick={()=>handleClike(8)} />
+      </div>
     </div>
   )
 }
