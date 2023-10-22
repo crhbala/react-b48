ca804cacedf839fea64f6e8e07990547051b9c6e
crhbala <chrisbala8888@gmail.com>
2023-10-22 10:25:30 +0530

    CRUD Creat and Read example

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,72 @@
-import React from 'react';
-import '../src/App.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
-function Button({value,onClick}) {
+function App(props) {
+    //define state
+    const [notes, setNotes] = useState(props.notes);
+    const [showStatus, setShowStatus] = useState('all');
+
+    const handleStatusChange = (event) => {
+        // console.log(event.target.value);
+        setShowStatus(event.target.value);
+    }
+
+    let filterNotes = (notes, showStatus) => {
+        switch (showStatus) {
+            case 'all':
+                return notes;
+            case 'imp':
+                return notes.filter(note => note.important === true);
+            case 'nonimp':
+                return notes.filter(note => note.important === false);
+        }
+    }
+    let notesFilter = filterNotes(notes, showStatus);
+    
+
   return (
-     <button className='square' onClick={onClick}>{value}</button>
-   )
-}
-
-function App() {
-
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-
-  function handleClike(i) {
-   
-    if (squares[i] || caculateWinner(squares)) {
-      return;
-    }
-
-    const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = 'X';
-    } else {
-      nextSquares[i] = 'O';
-    }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
-
-  }
-  
-  function caculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] == squares[b] && squares[a] == squares[c]) {
-        return squares[a];
-      }
-      
-    }
-
-    return null;
-  }
-
-  let winner = caculateWinner(squares);
-  let status;
-  if (winner) {
-    status = "Winner:" + winner;
-  } else {
-    status = 'Next player:' + (xIsNext ? 'X' : 'O');
-  }
-  
-  return (
-    <div>
       <div>
-        {status}
-      </div>
-      <div className='row'>
-        <Button value={squares[0]} onClick={()=>handleClike(0)} />
-        <Button value={squares[1]} onClick={()=>handleClike(1)} />
-        <Button value={squares[2]} onClick={()=>handleClike(2)} />
-      </div>
-      <div className='row'>
-        <Button value={squares[3]} onClick={()=>handleClike(3)} />
-        <Button value={squares[4]} onClick={()=>handleClike(4)} />
-        <Button value={squares[5]} onClick={()=>handleClike(5)} />
-      </div>
-      <div className='row'>
-        <Button value={squares[6]} onClick={()=>handleClike(6)} />
-        <Button value={squares[7]} onClick={()=>handleClike(7)} />
-        <Button value={squares[8]} onClick={()=>handleClike(8)} />
-      </div>
+          <h1>Notes</h1>
+
+          <label>
+              <input
+                  type="radio"
+                  name='filter'
+                  onChange={handleStatusChange}
+                  value='all'
+                  checked={showStatus==='all'}
+              />
+              All Notes
+          </label>
+
+          <label>
+              <input
+                  type="radio"
+                  name='filter'
+                  onChange={handleStatusChange}
+                  value='imp'
+                  checked={showStatus==='imp'}
+              />
+              Important Notes
+          </label>
+
+          <label>
+              <input
+                  type="radio"
+                  name='filter'
+                  onChange={handleStatusChange}
+                  value='nonimp'
+                  checked={showStatus==='nonimp'}
+              />
+              Non-Important Notes
+          </label>
+
+          <ul>
+              {
+                  notesFilter.map(note => 
+                      <li key={note.id}>{note.content}</li>
+                  )
+              }
+          </ul>
     </div>
   )
 }
