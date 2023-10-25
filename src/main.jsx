24a7714eee831b22e1d6d8f7e432d14ad29bf1de
crhbala <chrisbala8888@gmail.com>
2023-10-25 20:24:10 +0530

    viwe notes with redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 
-let initialState = {
-    count : 0
-}
-
-const counterReducer = (state = initialState.count, action)=>{
-    switch (action.type) {
-        case 'INCR':
-            return state + 1;
-        case 'DECR':
-            return state - 1;
-        case 'ZERO':
-            return 0;
-        default:
-            return state;
-    }
-}
-
-
-//create a new store
-const store = createStore(counterReducer);
-
-console.log(store.getState());
 
 ReactDOM.createRoot(document.getElementById('root')).render( <App />);
