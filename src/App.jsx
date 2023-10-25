9250ab23c6733a6c9eb1bb871e407a7b3b8a1495
crhbala <chrisbala8888@gmail.com>
2023-10-25 19:21:58 +0530

    redux store create counter access

--- src/App.jsx
+++ src/App.jsx
@@ -1,102 +1,21 @@
-import axios from 'axios';
-import React, { useEffect, useRef, useState } from 'react';
-import Dashboard from './Dashboard';
-import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import ReadNotes from './ReadNotes';
-import CreateNotes from './CreateNotes';
-import EditNote from './EditNote';
-import DeleteNote from './DeleteNote';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
-    //define state
-    const [notes, setNotes] = useState([]);
-    const [showStatus, setShowStatus] = useState('all');
 
-    //status for adding new note form
-    const [newNoteContant, setNewNoteContant] = useState('');
-    const [newNoteImportant, setNewNoteImportant] = useState('');
-
-    const newNoteContantRef = useRef(null);
-
-    const fetchNotes = async() => {
-        try {
-            const response = await axios.get('http://localhost:3000/notes/');
-            setNotes(response.data);
-        } catch (error) {
-            console.log('Failed to fetch notes:', error);
-        }
-    }
-
-    useEffect(() => {
-        fetchNotes();
-    }, []);
-
-    const addNote = (event) => {
-        event.preventDefault();
-        // console.log(newNoteContant,newNoteImportant);
-
-        //create a new note subject
-        let noteObject = {
-            id: notes.length + 1,
-            content: newNoteContant,
-            important: newNoteImportant === 'true',
-        }
-        // setNotes(notes.concat(noteObject));
-        console.log('adding new note....');
-        axios
-            .post('http://localhost:3000/notes/', noteObject)
-            .then(response => {
-            console.log('note added successfully....');
-        })
-
-        //clear the inputs
-        setNewNoteContant('');
-        setNewNoteImportant('');
-
-        newNoteContantRef.current.focus();
-        fetchNotes();
-    }
-
-
-    const handleStatusChange = (event) => {
-        // console.log(event.target.value);
-        setShowStatus(event.target.value);
-    }
-
-    let filterNotes = (notes, showStatus) => {
-        switch (showStatus) {
-            case 'all':
-                return notes;
-            case 'imp':
-                return notes.filter(note => note.important === true);
-            case 'nonimp':
-                return notes.filter(note => note.important === false);
-        }
-    }
-    let notesFilter = filterNotes(notes, showStatus);
-    
-    const padding = {
-        paddingRight : 15,
-    }
+    const dispatch = useDispatch();
+    const counter = useSelector(state => state);
 
   return (
-      <Router>
+      <div>
           <div>
-              <Link to="/" style={padding}>Dashboard</Link>
-              <Link to="/read" style={padding}>Read Notes</Link>
-              <Link to="/create" style={padding}>CreateNotes</Link>
-              <Link to="/edit" style={padding}>Edit Note</Link>
-              <Link to="/delete" style={padding}>Delete Note</Link>
+              {counter}
           </div>
-          <Routes>
-              <Route path='/' element={<Dashboard />} />
-              <Route path='/read' element={<ReadNotes showStatus={showStatus} handleStatusChange={handleStatusChange} notesFilter={notesFilter} />} />
-              <Route path='/create' element={<CreateNotes addNote={addNote} newNoteContant={newNoteContant} newNoteImportant={newNoteImportant} newNoteContantRef={newNoteContantRef} setNewNoteContant={setNewNoteContant} setNewNoteImportant={setNewNoteImportant} />} />
-              <Route path='/edit' element={<EditNote notes={notes} setNotes={ setNotes} />} />
-              <Route path='/delete' element={<DeleteNote notes={notes} setNotes={ setNotes} />} />
-              
-          </Routes>
-      </Router>
+          <button onClick={e => dispatch({ type: 'INCR' })}>Increment</button>
+          <button onClick={e => dispatch({ type: 'DECR' })}>Decrement</button>
+          <button onClick={e => dispatch({type: 'ZERO'})}>Zero</button>
+
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const counterReducer = (state = 0, action)=>{
+    switch (action.type) {
+        case 'INCR':
+            return state + 1;
+        case 'DECR':
+            return state - 1;
+        case 'ZERO':
+            return 0;
+        default:
+            return state;
+    }
+}
+
+const store = createStore(counterReducer);
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Provider store={store}>
+        <App />
+        </Provider>
+);
