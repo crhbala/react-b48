40610dbaaf94caa06aa803f37d7707aca73cee33
crhbala <chrisbala8888@gmail.com>
2023-10-26 16:15:58 +0530

    list toggle importance --not working

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,23 @@ import { createStore } from 'redux';
 
 const noteReducer = (state = [], action) => {
     if (action.type == 'NEW_NOTE') {
-        state.push(action.payload);
-        return state;
+        
+    }
+    switch (action.type) {
+        case 'NEW_NOTE':
+            return state.concat(action.payload);
+        case 'TOGGLE_IMPORTANCE':
+            //get the id from the action.payload
+            const id = action.payload.id;
+            const noteToChange = state.find(n => n.id === id);
+        // console.log(noteToChange);
+            const changedNote = {
+                ...noteToChange,
+                important: !noteToChange.important,
+            }
+            return state.map(note =>
+                note.id !==id ? note : changedNote
+                )
     }
     return state;
 }
@@ -30,13 +45,28 @@ store.dispatch({
 })
 
 function App() {
-    console.log(store.getState());
+    // console.log(store.getState());
+
+    const toggleImportance = (id) => {
+    console.log('clicked',id);
+    console.log();
+    store.dispatch({
+        type: 'TOGGLE_IMPORTANCE',
+        payload: {
+            id: id,
+        }
+     })
+}
+
   return (
       <div>
           <ul>
               {
                   store.getState().map(note =>
-                      <li key={note.id}>{note.content}<strong>{note.important ? '*' : '' }</strong></li>
+                      <li
+                        key={note.id}
+                        onClick={()=> toggleImportance(note.id)}
+                      >{note.content}<strong>{note.important ? '*' : ''}</strong></li>
                     )
               }
           </ul>
